dc157cf31e63e5638452406313d2702f53597bc1
Valentin Thevoz <valentin.thevoz@gmail.com>
2024-10-23 11:43:44 -0500

    Changed CLI to allow any ammount of elements

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ class Analysis_Element:
         self.element_number = element_number
 
     def input_data(self) -> None:
-        self.area = int(input(f"Enter area for Element_{self.element_number}: "))
-        self.length = int(input(f"Enter length for Element_{self.element_number}: "))
-        self.youngs_mod = int(input(f"Enter youngs modulus for Element_{self.element_number}: "))
+        self.area = float(input(f"Enter area for Element (m^2) {self.element_number}: "))
+        self.length = float(input(f"Enter length for Element (m) {self.element_number}: "))
+        self.youngs_mod = float(input(f"Enter youngs modulus for Element (kPa) {self.element_number}: "))
+        self.node_force = float(input(f"Enter the force applied on node (N) {self.element_number+1}: "))
 
     def generate_k(self) -> float:
         a = self.area
@@ -16,30 +17,26 @@ class Analysis_Element:
         self.k = (a*e)/l
         return self.k
 
+element_list = []
+num_elements = int(input("How many elements?: "))
+for i in range(num_elements):
+    element = Analysis_Element(i+1)
+    element.input_data()
+    element_list.append(element)
 
-element1 = Analysis_Element(1)
-element1.input_data()
-k1 = element1.generate_k()
+k_mat = np.zeros((num_elements+1,num_elements+1))
+F_vec = []
+for i in range(num_elements):
+    k_val = element_list[i].generate_k()
+    k_mat[i][i] += k_val
+    k_mat[i+1][i] = -k_val
+    k_mat[i][i+1] = -k_val
+    k_mat[i+1][i+1] += k_val
+    F_vec.append(element_list[i].node_force)
 
-element2 = Analysis_Element(2)
-element2.input_data()
-k2 = element2.generate_k()
-
-element3 = Analysis_Element(3)
-element3.input_data()
-k3 = element3.generate_k()
-
-k_mat = np.array(
-    [[k1+k2,-k2,0],
-    [-k2,k2+k3,-k3],
-    [0,-k3,k3]]
-)
-
-F_vec = np.zeros(3);
-
-for i in range(3):
-    F_vec[i] = int(input(f"Input force at node {i+2}: "))
+k_mat = np.delete(k_mat, (0), axis=0)
+k_mat = np.delete(k_mat, (0), axis=1)
 
 d = np.matmul(F_vec,np.linalg.inv(k_mat))
-d = np.insert(d,0,0)
+d.insert(0,0)
 print(d)
